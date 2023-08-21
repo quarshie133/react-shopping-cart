ef60b70424b9581cd2dd34976df0d8513d68c252
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-08-21 16:42:49 +0000

    fetch API and added typescript types

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 // Components
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
+// styles
+import { Wrapper } from "./App.styles";
+// Types
 
-const App =() => {
-  return (
-    <div className="App">
-     start
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
 
-    </div>
-  );
-}
+const getProducts = async () =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
+
+const App = () => {
+  return <div className="App">start</div>;
+};
 
 export default App;
