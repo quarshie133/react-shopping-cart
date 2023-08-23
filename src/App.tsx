212c9797dd46627c5937a71c8806361fbb0c682d
PrinceQuarshie <prince.quarshie@amalitech.org>
2023-08-23 14:58:36 +0000

    algorithm to remove items from cart

--- src/App.tsx
+++ src/App.tsx
@@ -39,23 +39,34 @@ const App = () => {
     items.reduce((ack: number, item) => ack + item.amount, 0);
 
   const handleAddToCart = (clickedItem: CartItemType) => {
-    setCartItems(prev => {
+    setCartItems((prev) => {
       // 1. is the item already added in the cart
-      const isItemInCart = prev.find(item => item.id === clickedItem.id)
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
 
       if (isItemInCart) {
-        return prev.map(item => 
-          item.id === clickedItem.id 
-          ? {...item, amount: item.amount + 1 }
-          : item
-        )
-      } 
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
       // First time the item is added
       return [...prev, { ...clickedItem, amount: 1 }];
-    })
-  }
+    });
+  };
 
-  const handleRemoveFromCart = () => null;
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[])
+    );
+  }
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something Went wrong...</div>;
@@ -63,10 +74,10 @@ const App = () => {
   return (
     <Wrapper>
       <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-        <Cart 
-        cartItems={cartItems} 
-        addToCart={handleAddToCart} 
-        removeFromCart={handleRemoveFromCart}
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
         />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
